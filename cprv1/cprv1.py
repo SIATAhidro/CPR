e24218bb89040a09e8b18063c86d000e6e00ce42
marcae777 <marcae7@gmail.com>
2018-02-23 16:40:36 -0500

    New class Nivel added, and some nwe functionality for SqlDb

--- cprv1/cprv1.py
+++ cprv1/cprv1.py
@@ -216,3 +216,221 @@ class SqlDb:
             else:
                 id    = pk[fecha]
                 self.update_data(field,value,id)
+
+    @staticmethod
+    def fecha_hora_query(start,end):
+        '''
+        Efficient way to query in tables with fields fecha,hora
+        such as table datos
+        Parameters
+        ----------
+        start        : initial date
+        end          : final date
+        Returns
+        ----------
+        Alternative query between two datetime objects
+        '''
+        start,end = pd.to_datetime(start),pd.to_datetime(end)
+        def f(date):
+            return tuple([date.strftime('%Y-%m-%d')]*2+[date.strftime('%H:%M:00')])
+        query = "("+\
+                "((fecha>'%s') or (fecha='%s' and hora>='%s'))"%f(start)+" and "+\
+                "((fecha<'%s') or (fecha='%s' and hora<='%s'))"%f(end)+\
+                ")"
+        return query
+
+    def fecha_hora_format_data(self,field,start,end):
+        '''
+        Gets pandas Series with data from tables with
+        date format fecha and hora detached, and filter
+        bad data
+        Parameters
+        ----------
+        field        : Sql table field name
+        start        : initial date
+        end          : final date
+        Returns
+        ----------
+        pandas time Series
+        '''
+        start,end = pd.to_datetime(start),pd.to_datetime(end)
+        format = (field,self.codigo,self.fecha_hora_query(start,end))
+        df = self.read_sql("SELECT fecha,hora,%s from datos WHERE cliente = '%s' and calidad = '1' and %s"%format)
+        # converts centiseconds in 0
+        df['hora'] = df['hora'].apply(lambda x:x[:-3]+':00')
+        # concatenate fecha and hora fields, and makes nan bad datetime indexes
+        df.index= pd.to_datetime(df['fecha'] + ' '+ df['hora'],errors='coerce')
+        df = df.sort_index()
+        # removes nan
+        df = df.loc[df.index.dropna()]
+        # mask duplicated index
+        df[df.index.duplicated(keep=False)]=np.NaN
+        df = df.dropna()
+        # drops coluns fecha and hora
+        df = df.drop(['fecha','hora'],axis=1)
+        # reindex to have all indexes in time series
+        new_index = pd.date_range(start,end,freq='min')
+        series = df.reindex(new_index)[field]
+        return series
+
+
+class Nivel(cpr.SqlDb,wmf.SimuBasin):
+    '''
+    Provide functions to manipulate data related
+    to a level sensor and its basin.
+    '''
+    def __init__(self,codigo = None,remote_server = info.REMOTE,**kwargs):
+        '''
+        The instance inherits modules to manipulate SQL
+        data and uses (hidrology modeling framework) wmf
+        Parameters
+        ----------
+        codigo        : primary key
+        remote_server :
+        local_server  : database kwargs to pass into the Sqldb class
+        '''
+        cpr.SqlDb.__init__(self,codigo=codigo,**kwargs)
+        simubasin = kwargs.get('path_nc')
+        if simubasin:
+            print 'setting wmf.SimuBasin'
+            wmf.SimuBasin.__init__(self,rute=simubasin)
+
+    @property
+    def infost(self):
+        '''
+        Gets full information from all stations
+        Returns
+        ---------
+        pd.DataFrame
+        '''
+        query = "SELECT * FROM %s"%(self.table)
+        return self.read_sql(query).set_index('Codigo')
+
+    @staticmethod
+    def get_radar_rain(start,end,path_nc,path_radar,path_save,
+                    converter = 'RadarConvStra2Basin2.py',
+                    utc=False,
+                    dt = 300):
+        '''
+        Convert radar rain to basin
+        Parameters
+        ----------
+        start         : inicial date
+        end           : final date
+        path_nc_basin : path to nc basin file
+        path_radar    : path to radar data
+        path_save     : path to save
+        converter     : path of main rain converter script,
+                        default RadarConvStra2Basin2.py
+        utc           : if radar data is in utc
+        dt            : timedelta, default = 5 minutes
+        Returns
+        ----------
+        bin, hdr files with rain data
+        '''
+        start = pd.to_datetime(start); end = pd.to_datetime(end)
+        if utc ==True:
+            delay = datetime.timedelta(hours=5)
+            start = start-delay
+            end = end - delay
+        hora_inicial = start.strftime('%H:%M')
+        hora_final = end.strftime('%H:%M')
+        format = (
+                converter,
+                start.strftime('%Y-%m-%d'),
+                end.strftime('%Y-%m-%d'),
+                path_nc,
+                path_radar,
+                path_save,
+                dt,
+                hora_inicial,
+                hora_final
+                 )
+        query = '%s %s %s %s %s %s -t %s -v -s -1 %s -2 %s'%format
+        output = os.system(query)
+        if output != 0:
+            print 'ERROR: something went wrong'
+        return query
+
+    @staticmethod
+    def hdr_to_series(path):
+        '''
+        Reads hdr rain files and converts it into pandas Series
+        Parameters
+        ----------
+        path         : path to .hdr file
+        Returns
+        ----------
+        pandas time Series with mean radar rain
+        '''
+        s =  pd.read_csv(path,skiprows=5,usecols=[2,3]).set_index(' Fecha ')[' Lluvia']
+        s.index = pd.to_datetime(map(lambda x:x.strip()[:10]+' '+x.strip()[11:],s.index))
+        return s
+
+    @staticmethod
+    def hdr_to_df(path):
+        '''
+        Reads hdr rain files and converts it into pandas DataFrame
+        Parameters
+        ----------
+        path         : path to .hdr file
+        Returns
+        ----------
+        pandas DataFrame with mean radar rain
+        '''
+        if path.endswith('.hdr') != True:
+            path = path+'.hdr'
+        df = pd.read_csv(path,skiprows=5).set_index(' Fecha ')
+        df.index = pd.to_datetime(map(lambda x:x.strip()[:10]+' '+x.strip()[11:],df.index))
+        df = df.drop('IDfecha',axis=1)
+        df.columns = ['record','mean_rain']
+        return df
+
+    @staticmethod
+    def bin_to_df(path,start=None,end=None,**kwargs):
+        '''
+        Reads rain fields (.bin) and converts it into pandas DataFrame
+        Parameters
+        ----------
+        path         : path to .hdr and .bin file
+        start        : initial date
+        end          : final date
+        Returns
+        ----------
+        pandas DataFrame with mean radar rain
+        Note
+        ----------
+        path without extension, ejm folder_path/file not folder_path/file.bin,
+        if start and end is None, the program process all the data
+        '''
+        start,end = pd.to_datetime(start),pd.to_datetime(end)
+        df = self.hdr_to_df(path)
+        if (start is not None) and (end is not None):
+            df = df.loc[start:end]
+        df = df[df['record']!=1]
+        records = df['record'].values
+        rain_field = []
+        for count,record in enumerate(records):
+            rain_field.append(wmf.models.read_int_basin('%s.bin'%path,record,self.ncells)[0])
+            count = count+1
+            format = (count*100.0/len(records),count,len(records))
+            print("progress: %.1f %% - %s out of %s"%format)
+        return pd.DataFrame(np.matrix(rain_field),index=df.index)
+
+    def mean_rain_vect(self,path):
+        pass
+
+    def mean_rain(self,start,end):
+        pass
+
+    def sensor(start,end,remote=False):
+        pass
+
+    def surface_velocity(start,end,remote=False):
+        pass
+
+    def camera(start,end):
+        pass
+
+    def pluvios_in_basin(start,end):
+        pass
