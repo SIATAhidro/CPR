d6861541bf1ff5f762608d6a20a98537ce37d939
marcae777 <marcae7@gmail.com>
2018-02-23 19:25:33 -0500

    fixed minor mistake

--- cprv1/information.py
+++ cprv1/information.py
@@ -104,7 +104,7 @@ LOCAL = {
                 }
 # argumentos por defecto para class SqlDb servidor de siata
 REMOTE = {
-	'host'  					:"192.168.1.74",
+	'host'  					:"localhost",
 	'user'  					:"sample_user",
 	'passwd'					:'s@mple_p@ss',
 	'table' 					: None,
